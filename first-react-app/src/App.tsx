d6b3df5131af35183787348e4b5ffb8594480b6b
Snehil <snehilwilldi@gmail.com>
2024-03-05 18:54:12 +0530

    State Management

--- first-react-app/src/App.tsx
+++ first-react-app/src/App.tsx
@@ -45,6 +45,54 @@ function App() {
     setDrink({ ...drink, price: 6 });
   };
 
+  //nested object update start
+
+  const [zip, setZip] = useState({
+    name: "John Cena",
+    address: {
+      city: "Ohio",
+      zipCode: 94111,
+    },
+  });
+
+  const zipHandler = () => {
+    setZip({ ...zip, address: { ...zip.address, zipCode: 94112 } });
+  };
+
+  // nested object update finished
+
+  // array update start
+
+  const [tags, setTags] = useState(["Happy", "Exciting"]);
+
+  const arrayHandler = () => {
+    //Add
+    setTags([...tags, "Sadness"]);
+    //Update
+    setTags(tags.map((value) => (value === "Happy" ? "Happiness" : value)));
+    //Remove
+    setTags(tags.filter((tag) => tag !== "Exciting"));
+  };
+
+  // array update ends
+
+  //Array of objects update start
+
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const arrayOfObjectHandler = () => {
+    //update -> suppose we have to list that bug 1 is fixed on button press
+    setBugs(
+      bugs.map((value) => (value.id === 1 ? { ...value, fixed: true } : value))
+    );
+  };
+  console.log(bugs);
+
+  //Array of objects update end
+
   return (
     <div>
       <ListGroup
@@ -64,6 +112,10 @@ function App() {
       <StrictMood />
       {drink.price}
       <button onClick={coffeeHandler}>Coffee</button>
+      {zip.address.zipCode}
+      <button onClick={zipHandler}>City Zip</button>
+      <button onClick={arrayHandler}>Array Update</button>
+      <button onClick={arrayOfObjectHandler}>Array Of Objects</button>
     </div>
   );
 }
@@ -71,6 +123,8 @@ function App() {
 export default App;
 
 //At line 18, we can pass children as Props, and due to this we can even use HTML elements inside the children components, But to use that we must change the props value from string to ReactNode. ReactNode lets us use HTML elements iniside a child.
+
+// UPDATING OBJECTS
 
 // Regarding the coffehandler and updating the drink price, we cannot directly go in the object and update it from there using drink.price = 6, No. We have to make a new object, copy the object values in that and update the changes there, and assign that value to the setState(newValue).
 
